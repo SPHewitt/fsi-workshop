33b7e926293e45f4955462c9a149e44ae07a17ea
Sam Hewitt <sam.hewitt@manchester.ac.uk>
2019-07-30 14:49:51 +0100

    Major: Adds edits for students to make
    This commit changes parameters in the files so that students can
    change them.

--- run/questions/HronTurek/fluid/plotDisplacement.py
+++ run/questions/HronTurek/fluid/plotDisplacement.py
@@ -6,27 +6,36 @@ if __name__ == '__main__':
     print("\n ---- Plotting Displacements ----\n")
 
     # File name
-    filename='history/0/point_487.dat'
+    filename='history/0/point_286.dat'
 
+    # Read in data file
     data=np.loadtxt(filename,skiprows=1)
 
+    # Seperate data
     time=data[:,0]
     dispX = data[:,1]
     dispY = data[:,2]
 
-
+    # Plotting routines
+    # Uncomment the three lines below and
+    # Try changing some of the parameters 
+    
+    #plt.title('My Title')
+    #plt.xlim(8,10)
+    #plt.ylim(-0.03,0.01)
     plt.xlabel('Time (s)')
     plt.ylabel('Displacment, X (m)')
     plt.grid(True)
-    plt.xlim(2,5)
     plt.plot(time,dispX,'-')
     plt.savefig('dispX.pdf',bbox_inches='tight')
     plt.close()
 
+    #plt.title('My Title')
+    #plt.xlim(8,10)
+    #plt.ylim(-0.08,0.08)
     plt.xlabel('Time (s)')
     plt.ylabel('Displacement, Y (m)')
     plt.grid(True)
-    plt.xlim(2,5)
     plt.plot(time,dispY,'-')
     plt.savefig('dispY.pdf',bbox_inches='tight')
     plt.close()
--- run/questions/HronTurek/fluid/plotForces.py
+++ run/questions/HronTurek/fluid/plotForces.py
@@ -13,21 +13,28 @@ if __name__ == '__main__':
     data=np.loadtxt(filename)
     time = data[:,0]
     forceX = data[:,1]
-    forceY = data[:,2] 
-
-
+    forceY = data[:,2]
+    
+    # Plotting routines
+    # Uncomment the three lines below and
+    # Try changing some of the parameters 
+    
+    #plt.title('My Title')
+    #plt.xlim(8,10)
+    #plt.ylim(-200,200)
     plt.xlabel('Time (s)')
     plt.ylabel('Force, X (N)')
     plt.grid(True)
     plt.plot(time,forceX,'-')
-    plt.xlim(2,5)
     plt.savefig('forceX.pdf',bbox_inches='tight')
     plt.close()
 
+    #plt.title('My Title')
+    #plt.xlim(8,10)
+    #plt.ylim(-200,200)
     plt.xlabel('Time (s)')
     plt.ylabel('Force, Y (N)')
     plt.grid(True)
-    plt.xlim(2,5)
     plt.plot(time,forceY,'-')
     plt.savefig('forceY.pdf',bbox_inches='tight')
     plt.close()
